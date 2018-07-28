1f59862161c271edf4401f13bb5ce6238555181c
saifdev1642 <saifdev1642@gmail.com>
2018-07-28 12:25:48 +0600

    Added search controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,29 @@
-import axios from 'axios';
+import Search from './models/Search';
+/** Global State of the Object
+ * - Search object
+ * - current recipee object
+ * - shopping list object
+ * - Liked object
+ */
 
-async function getResults(query){
-    try{
-    const proxy = 'https://cors-anywhere.herokuapp.com/'
-    const key = '72168958be9ff320473c32c60a20a9e6';
-    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-    console.log(res);
-    }catch(error){
-        alert(error);
+const state = {};
+
+const searchController = async ()=>{
+    //1.Get Query from the view
+    const query = 'pizza';
+    if(query){
+        //2. New search object and add to state
+        state.search = new Search(query);
+        //3. prepare UI for results
+        //4. Search for recipee
+        await state.search.getResults();
+        //5. output result to UI
+        console.log(state.search.result);
     }
 }
 
-getResults('pizza');
+
+document.querySelector('.search').addEventListener('submit',e=>{
+    e.preventDefault();
+    searchController();
+})

7e5846b555868eeb309ac9bb3bb34ea34db647bc
saifdev1642 <saifdev1642@gmail.com>
2018-07-28 10:49:51 +0600

    Make Api Call

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,14 @@
-// Global app controller
-import x from './test';
-const num=555;
-console.log(`imported variable value is ${x} number is ${num}`);
+import axios from 'axios';
+
+async function getResults(query){
+    try{
+    const proxy = 'https://cors-anywhere.herokuapp.com/'
+    const key = '72168958be9ff320473c32c60a20a9e6';
+    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+    console.log(res);
+    }catch(error){
+        alert(error);
+    }
+}
+
+getResults('pizza');
